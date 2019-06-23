1e8067ce3148dd6adcf619da390585f039d446b1
Goufix <alifer.17.11@gmail.com>
2019-06-23 19:48:41 -0300

    Luiz-CSS

--- src/components/Repo/index.tsx
+++ src/components/Repo/index.tsx
@@ -5,7 +5,7 @@ export default function Repo() {
   return (
     <Container>
       <ProfilePhoto src="https://avatars1.githubusercontent.com/u/23662020?v=4" />
-      <User> Luiz Felipe Gonçalves </User>
+      <User> lffg / adonis-api-boilerplate </User>
     </Container>
   );
 }
--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -1,10 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-* {
-  margin: 0;
-  border: 0;
-  border-style: border-box;
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  min-height: 100vh;
+}
+
+body {
+  background-color: #fff;
+  color: #212121;
+  line-height: 1.5;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
+    sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+  font-size: 17px;
+}
 }
 `;
 
